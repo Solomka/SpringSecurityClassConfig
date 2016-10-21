c9ca1f97042402e0ffffa401ba8f52242d49446b
Solomka <sol.yaremko@gmail.com>
2016-10-22 02:05:21 +0300

    final commit

---
+++ src/main/java/ua/com/yaremko/springsecurity/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package ua.com.yaremko.springsecurity.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
